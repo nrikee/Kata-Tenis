244c26f1fb9a34ded419dcd9b1d484d017c468f6
nRikee <nrikee16@gmail.com>
2013-01-12 07:45:14 -0800

    Create Jugador.java

---
+++ Jugador.java
@@ -0,0 +1,55 @@
+public class Jugador{
+    String nombreJugador;
+    int puntuacion;
+     
+    final int GANA = 4;
+    final int DEUCE = 5;
+    final int VENTAJA = 6;
+    final int[] marcador = {0,15,30,40,-1,-2,-3,-1};
+     
+    /** Constructor */
+    public Jugador(){
+        this("Player 1");
+    }
+     
+    /** Constructor */
+    public Jugador(String n){
+        nombreJugador = n;
+        puntuacion = 0;
+    }
+     
+    /** Sube la puntuación del marcador */
+    public void anota(){ puntuacion++; }
+     
+    /** Comprueba si está en Deuce */
+    public boolean isDeuce(){ return puntuacion==DEUCE; }
+     
+    /** Pon la puntuación en Deuce */
+    public void setDeuce(){ puntuacion = DEUCE; }
+     
+    /** Resetea la puntuación */
+    public void reset(){ puntuacion = 0; }
+     
+    /** Obtener puntuación del jugador */
+    public int getPuntuacion(){ return marcador[puntuacion]; }
+     
+    /** Obtener la puntuación en forma de String */
+    public String getStringPuntuacion(){
+        String str;
+         
+        switch(getPuntuacion()){
+            case -1: str = "SET"; break;
+            case -2: str = "DEUCE"; break;
+            case -3: str = "VENTAJA"; break;
+            default: str = "";
+        }
+         
+        return "" + (str.equals("")? getPuntuacion(): str);
+    }
+     
+    /** Sobreescritura de toString() */
+    public String toString(){    
+        return nombreJugador;
+    }
+     
+}
